42d2dafdaad655252f6cb1cf2040eacee62f7b23
Dev Rangoonwala <devcodes77@gmail.com>
2020-10-16 14:53:47 +0530

    Add files via upload

---
+++ basicDataType.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int it;
+    long lng;
+    char ch;
+    float fot;
+    double dob;
+    scanf("%d %ld %c %f %lf", &it, &lng, &ch, &fot, &dob);
+    printf("%d\n%ld\n%c\n%f\n%lf", it, lng, ch, fot, dob);
+    return 0;
+}
